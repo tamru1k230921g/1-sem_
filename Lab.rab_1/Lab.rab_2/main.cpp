e34122c1d921d64f3d7c60a40cede095b0d75700
_t_am_ru_ <91288139+tamru1k230921g@users.noreply.github.com>
2022-03-21 14:43:03 +0300

    Add files via upload

---
+++ Lab.rab_1/Lab.rab_2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+void main()
+{
+	float x = 3, y; a=1.75 , b=4.5*10;
+	;
+	std::cout << "t=" << t;
+	std::cout << "u=" << u;
+}
---
+++ lab.rab_0/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <chrono> //для работы с таймером
+using namespace std;
+
+void timer()
+{
+	
+	int hours = 0, minutes = 0, seconds = 0,n;
+	cout << "Введите время таймера(в секундах)";
+	cin >> n;
+	for (int seconds = 0; 1 < n;++seconds) {
+		cout << hours << ":" << minutes << ":" << seconds;
+		if (seconds == 60) {
+			seconds = 0;
+			minutes++;
+		}
+		if (minutes == 60) {
+			minutes = 0;
+			hours++;
+		}
+		if (seconds == n) {
+			system("cls"); //очистка консоли
+		}
+		system("cls"); //очистка консоли
+	}
+	return 0;
+}
+int main()
+{
+
+	void timer();
+	return 0;
+
+}
